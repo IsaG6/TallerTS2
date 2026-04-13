c50cc74b0cf3f1c8686ce43fe377b6091debcea0
IsaG6 <i.gonzalezl@uniandes.edu.co>
2026-04-12 20:05:40 -0500

    Add files via upload

---
+++ TallerTypeScript2/dist/main.js
@@ -0,0 +1,40 @@
+import { series } from "./data.js";
+const table = document.getElementById("series-table");
+const avgText = document.getElementById("average");
+const cardContainer = document.getElementById("series-card");
+let totalSeasons = 0;
+series.forEach((serie) => {
+    let row = document.createElement("tr");
+    row.className = "fila-serie";
+    row.style.cursor = "pointer";
+    row.innerHTML = `
+        <td><strong>${serie.id}</strong></td>
+        <td><a class="serie-link">${serie.name}</a></td>
+        <td>${serie.channel}</td>
+        <td>${serie.seasons}</td>
+    `;
+    row.onclick = (e) => {
+        e.preventDefault();
+        showDetail(serie);
+    };
+    table.appendChild(row);
+    totalSeasons += serie.seasons;
+});
+//promedio
+let avg = totalSeasons / series.length;
+avgText.innerHTML = `<strong>Seasons average: ${avg}</strong>`;
+function showDetail(serie) {
+    cardContainer.innerHTML = `
+        <div class="card">
+            <img src="${serie.image}" 
+                 class="card-img-top" 
+                 alt="${serie.name}" 
+                 style="height: auto; max-height: 300px; object-fit: contain; background-color: #eee;">
+            <div class="card-body">
+                <h5 class="card-title"><strong>${serie.name}</strong></h5>
+                <p class="card-text text-secondary">${serie.description}</p>
+                <a href="${serie.link}" target="_blank" class="text-primary" style="word-break: break-all;">${serie.link}</a>
+            </div>
+        </div>
+    `;
+}
